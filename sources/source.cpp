73376762a53abfe0b236262b76bb44501a89c7d3
gnole <olegbouchin@gmail.com>
2020-09-09 18:28:59 +0300

    add cod

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,19 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+int main() {
+    //...
+    std::ifstream file{jsonPath};
+    if (!file) {
+        throw std::runtime_error{"unable to open json: " + jsonPath};
+    }
+
+    json data;
+    file >> data;
+
+    std::vector<Student> students;
+    for (auto const& student : data.at("items")) {
+        students.push_back(Student{student});
+    }
+    //...
+}
